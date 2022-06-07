94715a28859cc7e44a10362e314468f909c60272
Kirilova <lisenok1717@inbox.ru>
2022-06-07 11:34:28 +0300

    Java3

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amountInKopecks = 1000000;
+        int percentInKopecks = 2000;
+        int bounce = amountInKopecks / percentInKopecks;
+        System. out. println("Ваш бонус составляет:"+ bounce +" мили");
+    }
+
+}
